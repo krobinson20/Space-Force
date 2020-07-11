9503736517fb6c7486be38ab68963bbddd2d060b
Humanphyti <humanphyti@gmail.com>
2020-07-10 19:35:14 -0700

    Finished Ship
    Added ShipStats and finished ShipBrain as well as starting BarMath

---
+++ Assets/Scripts/Ship/ShipStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipStats
+{
+    [SerializeField]
+    private float initialDescentSpeed;
+    public float InitialDescentSpeed
+    {
+        get { return initialDescentSpeed; }
+    }
+
+    [SerializeField]
+    private float initialFuel;
+    public float InitialFuel
+    {
+        get { return initialFuel; }
+    }
+
+    [SerializeField]
+    private float initialPower;
+    public float InitialPower
+    {
+        get { return initialPower; }
+    }
+
+    [SerializeField]
+    private float initialIntegrity;
+    public float InitialIntegrity
+    {
+        get { return initialIntegrity; }
+    }
+}
